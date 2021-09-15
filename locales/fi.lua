42a637ad9e73228eb05bac0e3473edc565ef1d31
RosdeeStudio <rosdee.studio@gmail.com>
2021-09-15 21:39:11 +0300

    first commit

---
+++ locales/fi.lua
@@ -0,0 +1,9 @@
+Locales['fi'] = {
+    ["mission_row"] = "Poliisi",
+    ["phone_booth"] = "Puhelinkioski",
+    ["key_answer"] = "Paina ~INPUT_PICKUP~ vastataksesi puheluun",
+    ["new_message"] = "~o~Uusi viesti",
+    ["new_message_from"] = "~o~Uusi viesti lähettäjältä ~y~%s",
+    ["new_message_transmitter"] = "~o~Uusi viesti lähettäjältä ~g~%s",
+    ["use_fixed"] = "~g~%s's phone ~o~(%s) ~n~~INPUT_PICKUP~~w~ Use phone"
+}
